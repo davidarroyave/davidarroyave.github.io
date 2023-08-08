610b93f424f919eb6ce72dcf7ef01bacbccc9c73
J. David Arroyave <59174198+davidarroyave@users.noreply.github.com>
2023-08-07 21:40:44 -0500

    Update script.js for practice english

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
-//window.alert("This page is in spanish, if you want to continue with english please try with the Google translate option")
-//PRACTICE IN DIFFERENT CONTEXTS II
 
 function init() {
+    if (window.location.pathname === 'practiceenglish.html') { //replace this on GITHUB
     var count = 1; // set secconds
     var counter = setInterval(timer, 5000 * count);
 
@@ -10,11 +9,12 @@ function init() {
         alert("This page is in spanish, if you want to continue with english please try with the translation option")
         window.location = "#";
         clearInterval(counter) // stop interval
+        }
+
     }
-
 }
+window.onload = init;  
 
-window.onload = init;
 
 var fullimgbox = document.getElementById("fullimgbox");
 var fullimg = document.getElementById("fullimg");
@@ -25,9 +25,22 @@ function openfullimg(pic){
 }
 
 function closefullimg(){
-    fullimgbox.style.display = "none";
-    
+    fullimgbox.style.display = "none";  
 }
 
-////////////////////
+//////accordion
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
